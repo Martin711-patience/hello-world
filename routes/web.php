a2742892dbc31c0d6ba7c45b1d90c33e5c1dad08
Martin Patience <patiencemartin954@gmail.com>
2025-07-10 18:05:00 +0200

    MVC Workflow

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('contact', function () {
     return view('contact');
